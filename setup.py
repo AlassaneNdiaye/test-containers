68aa957c557279f75ad1eba0a84d8792bfec81c6
Alassane Ndiaye <alassane.ndiaye@gmail.com>
2019-11-26 15:57:29 -0500

    Updated setup.py

--- setup.py
+++ setup.py
@@ -6,19 +6,18 @@ dir_path = os.path.dirname(os.path.abspath(__file__))
 with open(os.path.join(dir_path, "test_containers/help.txt"), "r") as f:
     help_text = f.read()
 
-with open(os.path.join(dir_path, "README.md"), "r") as f:
-    long_description = f.read()
-
 setuptools.setup(
     name="test-containers",
-    version="0.1",
+    version="0.1.9",
     author="Alassane Ndiaye",
     author_email="alassane.ndiaye@gmail.com",
     description=help_text,
-    long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/AlassaneNdiaye/test-containers",
     packages=setuptools.find_packages(),
+    package_data={
+        "": ["*.txt"],
+    },
     install_requires=[
         "docker",
         "kubernetes",
